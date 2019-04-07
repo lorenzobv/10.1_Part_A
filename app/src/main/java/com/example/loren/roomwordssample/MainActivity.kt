32aa38508bcb9caaf0ee5352f7d5aed0224d23ff
loren <lawrencebonnickvilela905@gmail.com>
2019-04-07 20:21:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/loren/roomwordssample/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.loren.roomwordssample
+
+import android.app.Activity
+import android.arch.lifecycle.Observer
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.arch.lifecycle.ViewModelProviders
+import android.support.annotation.Nullable
+import android.content.Intent
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.Toast
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    val NEW_WORD_ACTIVITY_REQUEST_CODE = 1
+    private var mWordViewModel: WordViewModel? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Get a new or existing ViewModel from the ViewModelProvider.
+        mWordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
+
+        // Add an observer on the LiveData returned by getAlphabetizedWords.
+        // The onChanged() method fires when the observed data changes and the activity is
+        // in the foreground.
+        mWordViewModel!!.getAllWords().observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            adapter.setWords(words)
+        })
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE)
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val word = Word(data!!.getStringExtra(NewWordActivity.EXTRA_REPLY))
+            mWordViewModel!!.insert(word)
+        } else {
+            Toast.makeText(
+                    applicationContext,
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show()
+        }
+    }
+}
